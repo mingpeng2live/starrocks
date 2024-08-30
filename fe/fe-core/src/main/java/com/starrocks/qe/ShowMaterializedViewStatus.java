56653ea5c1b9ca786e209fc36112b12e7370eaec
shuming.li <ming.moriarty@gmail.com>
2024-08-30 13:22:26 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowMaterializedViewStatus.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowMaterializedViewStatus.java
@@ -201,14 +201,6 @@ public class ShowMaterializedViewStatus {
             this.extraMessage = extraMessage;
         }
 
-        public String getTaskOwner() {
-            return taskOwner;
-        }
-
-        public void setTaskOwner(String taskOwner) {
-            this.taskOwner = taskOwner;
-        }
-
         @Override
         public String toString() {
             return "RefreshJobStatus{" +
